3f92532ac411fed8ab0ab6e65e7f48d2b6d7544b
Caio Lucius <caiolucius2010@hotmail.com>
2023-11-19 16:41:59 -0300

    PEDIDOS ADICIONADO
    PEDIDOS ADICIONADO

--- app/main.py
+++ app/main.py
@@ -4,6 +4,8 @@ from fastapi.responses import JSONResponse
 from sqlalchemy.orm import Session
 from app import models, database
 from pydantic import BaseModel
+from sqlalchemy import Boolean
+from typing import List
 
 app = FastAPI()
 
@@ -16,27 +18,59 @@ class ItemCreate(BaseModel):
     estoque: int
     preco: float  
 
+class UsuarioCreate(BaseModel):
+    nome: str
+    email: str
+    senha: str
+    is_admin: bool
+
+class PedidoCreate(BaseModel):
+    item_id: int
+    quantidade: int
+
+class PedidoResponse(BaseModel):
+    id: int
+    item_id: int
+    quantidade: int
+    usuario_id: int
+
+    class Config:
+        orm_mode = True
+
+
 # Função para popular o banco de dados com dados de exemplo.
 def seed_data():
     db = database.SessionLocal()
     db.query(models.ItemDB).delete()
-    products = [
-        {'nome': "Nike Air", "descricao": "Sapato clássico e estiloso.", "estoque": 10, "preco": 499.99},
-        {'nome': "Nike Pine Green", "descricao": "Design moderno em verde.", "estoque": 8, "preco": 799.99},
-        {'nome': "Nike Air Jordan", "descricao": "Estilo lendário do basquete.", "estoque": 12, "preco": 899.99},
-        {'nome': "Nike Cactus", "descricao": "Toque de ousadia e estilo.", "estoque": 7, "preco": 1299.99},
-        {'nome': "Nike Booster", "descricao": "Eleva seu estilo a outro nível.", "estoque": 10, "preco": 1199.99},
-        {'nome': "Nike Yeezy", "descricao": "Design único e moderno.", "estoque": 20, "preco": 2499.99},
-        {'nome': "Nike Downshifter", "descricao": "Conforto para o dia a dia.", "estoque": 18, "preco": 299.99},
-        {'nome': "Nike Revolution", "descricao": "Performance e estilo em um só.", "estoque": 14, "preco": 449.99},
-        {'nome': "Nike Flex", "descricao": "Flexibilidade para seus pés.", "estoque": 6, "preco": 349.99}, 
-        {'nome': "Nike Saturn", "descricao": "Conforto de um astronauta", "estoque": 5, "preco": 340.99}, 
+    db.query(models.UsuarioDB).delete()
+
+    users = [
+    {'nome': "Adailton Lima Segundo", "email": "adlima@bol.com",        "senha": "adlima2023",  "is_admin": False},
+    {'nome': "Gabriel Badas",         "email": "gbadas@globomail.com",  "senha": "gbadas2023",  "is_admin": True},
+    {'nome': "Lucius Nascimento",     "email": "lucius2010@yahoo.com",  "senha": "clucius2023", "is_admin": False},
+    {'nome': "Professor Xavier",      "email": "xaviersisdb@gmail.com", "senha": "gbadas2023",  "is_admin": True},
+    {'nome': "Vivian Santana",        "email": "adv.vivi@gov.br",       "senha": "clucius2023", "is_admin": False},
     ]
-    for product in products:
-        # Adição da chamada da função models.ItemDB.create no loop for
+    for user in users:
+        models.UsuarioDB.create(db, models.UsuarioCreate(**user))
+
+    products = [
+    {'nome': "Tilápia Fresca",        "descricao": "Tilápia fresca, perfeita para grelhados e assados.",               "estoque": 10,  "preco": 9.99 },
+    {'nome': "Atum Selvagem",         "descricao": "Atum selvagem, ideal para sashimi e pratos de peixe cru.",         "estoque": 8,   "preco": 19.99},
+    {'nome': "Salmão do Atlântico",   "descricao": "Salmão do Atlântico, rico em ácidos graxos ômega-3.",              "estoque": 12,  "preco": 14.99},
+    {'nome': "Linguado Fresco",       "descricao": "Linguado fresco, delicado e perfeito para pratos gourmet.",        "estoque": 7,   "preco": 24.99},
+    {'nome': "Truta Arco-Íris",       "descricao": "Truta arco-íris, saborosa e versátil em várias receitas.",         "estoque": 10,  "preco": 12.99},
+    {'nome': "Bacalhau do Atlântico", "descricao": "Bacalhau do Atlântico, clássico em pratos de bacalhau.",           "estoque": 20,  "preco": 29.99},
+    {'nome': "Robalo Fresco",         "descricao": "Robalo fresco, excelente para grelhados e pratos assados.",        "estoque": 18,  "preco": 16.99},
+    {'nome': "Peixe-gato de Cultivo", "descricao": "Peixe-gato de cultivo, ideal para frituras e ensopados.",          "estoque": 14,  "preco": 8.99 },
+    {'nome': "Lula Limpa",            "descricao": "Lula limpa, pronta para preparar pratos de frutos do mar.",        "estoque": 6,   "preco": 22.99}, 
+    {'nome': "Camarão Fresco",        "descricao": "Camarão fresco, ótimo para pratos como paella e camarão ao alho.", "estoque": 5,   "preco": 18.99}, 
+    ]
+    
+    for product in products: 
         item = models.ItemDB.create(db, product["nome"], product["descricao"], product["estoque"], product["preco"])
         print(f"Item criado: {item}")
-    # Fecha a sessão ao final
+
     db.close()
 # Chama a função seed_data sempre que o aplicativo for iniciado ou reiniciado
 seed_data()
@@ -79,8 +113,74 @@ def delete_item(item_id: int, db: Session = Depends(database.get_db)):
         raise HTTPException(status_code=404, detail="Item não encontrado.")
     return JSONResponse(content={"message": "Item excluído."})
 
-# Endpoint para popular o banco de dados com dados de exemplo.
-@app.post("/estoque/seed-data/", response_model=dict)
-def seed_data_endpoint(db: Session = Depends(database.get_db)):
-    seed_data(db)
-    return {"message": "Dados de exemplo adicionados ao banco de dados."}
+# ROTAS CLIENTES ####################################################################################################################################################
+# Rota para criar um novo usuário
+@app.post("/usuarios/adicionar/", response_model=models.Usuario)
+def create_usuario(usuario_create: UsuarioCreate, db: Session = Depends(database.get_db)):
+    return models.UsuarioDB.create(db, usuario_create)
+
+# Rota para obter todos os usuários
+@app.get("/usuarios/", response_model=List[models.Usuario])
+def read_usuarios(skip: int = 0, limit: int = 10, db: Session = Depends(database.get_db)):
+    return models.UsuarioDB.get_all(db, skip=skip, limit=limit)
+
+# Rota para obter um usuário por ID
+@app.get("/usuarios/{usuario_id}", response_model=models.Usuario)
+def read_usuario(usuario_id: int, db: Session = Depends(database.get_db)):
+    usuario = models.UsuarioDB.get_by_id(db, usuario_id)
+    if usuario is None:
+        raise HTTPException(status_code=404, detail="Usuário não encontrado.")
+    return usuario
+
+# Rota para atualizar um usuário por ID
+@app.put("/usuarios/atualizar/{usuario_id}", response_model=models.Usuario)
+def update_usuario(usuario_id: int, usuario_update: UsuarioCreate, db: Session = Depends(database.get_db)):
+    updated_usuario = models.UsuarioDB.update(db, usuario_id, usuario_update)
+    if updated_usuario is None:
+        raise HTTPException(status_code=404, detail="Usuário não encontrado.")
+    return updated_usuario
+
+# Rota para excluir um usuário por ID
+@app.delete("/usuarios/remover/{usuario_id}", response_model=models.Usuario)
+def delete_usuario(usuario_id: int, db: Session = Depends(database.get_db)):
+    deleted_usuario = models.UsuarioDB.delete(db, usuario_id)
+    if deleted_usuario is None:
+        raise HTTPException(status_code=404, detail="Usuário não encontrado.")
+    return {"message": "Usuário excluído."}
+
+# OPERAÇÕES GERAIS ################################################################################################################################################
+# Rota para fazer um pedido de compra
+@app.post("/pedido/adicionar/{usuario_id}", response_model=PedidoResponse)
+def add_pedido(usuario_id: int, pedido_create: PedidoCreate, db: Session = Depends(database.get_db)):
+    # Verifica se o usuário é válido
+    usuario = models.UsuarioDB.get_by_id(db, usuario_id)
+    if not usuario:
+        raise HTTPException(status_code=404, detail="Usuário não encontrado.")
+
+    # Verifica se o item existe
+    item = models.ItemDB.get_by_id(db, pedido_create.item_id)
+    if not item:
+        raise HTTPException(status_code=404, detail="Item não encontrado.")
+
+    # Calcula o valor total do pedido
+    valor_total = item.preco * pedido_create.quantidade
+
+    # Atualiza o estoque
+    if item.estoque >= pedido_create.quantidade:
+        item.estoque -= pedido_create.quantidade
+        db.commit()
+
+        # Cria o pedido associado ao usuário
+        pedido = models.PedidoDB(
+            item_id=pedido_create.item_id,
+            quantidade=pedido_create.quantidade,
+            usuario_id=usuario_id
+        )
+        db.add(pedido)
+        db.commit()
+
+        print(f"Pedido recebido de {usuario.nome}. Valor total: {valor_total}")
+
+        return pedido
+    else:
+        raise HTTPException(status_code=400, detail="Quantidade solicitada maior do que o estoque disponível.")
--- app/models.py
+++ app/models.py
@@ -1,20 +1,22 @@
 # app/models.py
-from sqlalchemy import Column, Integer, String, Float
+from sqlalchemy import Column, Integer, String, Float, Boolean, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session, relationship
 from pydantic import BaseModel
 
 Base = declarative_base()
 
-class Item(BaseModel):
-    id: int
+class ItemBase(BaseModel):
     nome: str
     descricao: str
     estoque: int
     preco: float
 
+class Item(ItemBase):
+    id: int
+
     class Config:
-        orm_mode = True
+        from_attributes = True
 
 class ItemDB(Base):
     __tablename__ = "items"
@@ -23,6 +25,9 @@ class ItemDB(Base):
     descricao = Column(String)
     estoque = Column(Integer)
     preco = Column(Float)
+
+    # Relacionamento com a tabela de pedidos
+    pedidos = relationship("PedidoDB", back_populates="item")
 
     @classmethod
     def create(cls, db: Session, nome: str, descricao: str, estoque: int, preco: float):
@@ -59,3 +64,88 @@ class ItemDB(Base):
             db.delete(item)
             db.commit()
         return item
+
+class UsuarioBase(BaseModel):
+    nome: str
+    email: str
+    senha: str
+    is_admin: bool
+
+class UsuarioCreate(UsuarioBase):
+    pass
+
+class Usuario(UsuarioBase):
+    id: int
+
+    class Config:
+        from_attributes = True
+
+class UsuarioDB(Base):
+    __tablename__ = "usuarios"
+    id = Column(Integer, primary_key=True, index=True)
+    nome = Column(String, index=True)
+    email = Column(String, unique=True, index=True)
+    senha = Column(String)
+    is_admin = Column(Boolean)
+
+    # Relacionamento com a tabela de pedidos
+    pedidos = relationship("PedidoDB", back_populates="usuario")
+
+    @classmethod
+    def create(cls, db: Session, usuario_create: UsuarioCreate):
+        usuario = cls(**usuario_create.dict())
+        db.add(usuario)
+        db.commit()
+        db.refresh(usuario)
+        return usuario
+
+    @classmethod
+    def get_all(cls, db: Session, skip: int = 0, limit: int = 10):
+        return db.query(cls).offset(skip).limit(limit).all()
+
+    @classmethod
+    def get_by_id(cls, db: Session, usuario_id: int):
+        return db.query(cls).filter(cls.id == usuario_id).first()
+
+    @classmethod
+    def update(cls, db: Session, usuario_id: int, usuario_update: UsuarioCreate):
+        usuario = cls.get_by_id(db, usuario_id)
+        if usuario:
+            for key, value in usuario_update.dict().items():
+                setattr(usuario, key, value)
+            db.commit()
+            db.refresh(usuario)
+        return usuario
+
+    @classmethod
+    def delete(cls, db: Session, usuario_id: int):
+        usuario = cls.get_by_id(db, usuario_id)
+        if usuario:
+            db.delete(usuario)
+            db.commit()
+        return usuario
+    
+class PedidoResponse(BaseModel):
+    id: int
+    item_id: int
+    quantidade: int
+    usuario_id: int
+
+    class Config:
+        orm_mode = True
+
+class PedidoCreate(BaseModel):
+    item_id: int
+    quantidade: int
+
+class PedidoDB(Base):
+    __tablename__ = "pedidos"
+    id = Column(Integer, primary_key=True, index=True)
+    item_id = Column(Integer, ForeignKey("items.id"))
+    quantidade = Column(Integer)
+    usuario_id = Column(Integer, ForeignKey("usuarios.id"))
+
+    # Relacionamento com a tabela de usuários
+    usuario = relationship("UsuarioDB", back_populates="pedidos")
+    # Relacionamento com a tabela de itens
+    item = relationship("ItemDB", back_populates="pedidos")
